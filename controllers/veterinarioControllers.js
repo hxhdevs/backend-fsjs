7916b4222f24dc6e92db3353636d2ce68b7343bf
HXHdevs <jordisonnicolas@gmail.com>
2024-07-29 23:48:13 -0600

    Almacenando datos en la BD

--- controllers/veterinarioControllers.js
+++ controllers/veterinarioControllers.js
@@ -1,12 +1,18 @@
-const registrar = (req,res) => {
-    console.log(req.body)
-    const{nombre,email,password}=req.body
-    console.log(nombre)
-    console.log(email)
-    console.log(password)
+import Veterinario from '../models/Veterinario.js'; 
 
-    res.json({msg:"Registrando usuario"});
-}
+const registrar = async (req,res) => {    
+    // const{nombre,email,password}=req.body
+
+    try {
+        //Guardar un nuevo veterinario
+        const veterinario = new Veterinario(req.body);
+        const veterinarioGuardado = await veterinario.save();
+        
+        res.json(veterinarioGuardado);
+    } catch (error) {
+        console.log(error)
+    }
+};
 const perfil =(req,res) => {
     res.json({msg:"Mostrando perfil"});
 }
